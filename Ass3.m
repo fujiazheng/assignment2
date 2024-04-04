478b7831d2e3b6ab60b9e327eda514732ed3f2a5
fujiazheng <163454747+fujiazheng@users.noreply.github.com>
2024-04-04 09:50:57 -0400

    Update Ass3.m
    updated with comparing distance

--- Ass3.m
+++ Ass3.m
@@ -2,13 +2,12 @@ clear;
 clc;
 close all;
 % initialization of the parameters for the function
-global mat sigma   
-mat = 15;                                      
-sigma = 3;
 numFrame=18;
 position=cell(1,numFrame);
 %initialize the image array
 frame=zeros(512,512,numFrame-1);
+%index variable copy the index in different frames
+index=zeros(numFrame,12);
 
 denoisedFrame=zeros(512,512,numFrame);
 denoisedFrame2=zeros(512,512,numFrame);
@@ -41,6 +40,12 @@ for i=1:numFrame
         objectCrop{i,j} = num2cell(imcrop(denoisedFrame2(:,:,i), bounding(j,:).BoundingBox));
         
         if i > 1 
+        %% Compare the distance
+            dist= (centroids(j,1,i)-centroids(:,1,i-1)).^2 + (centroids(j,2,i)-centroids(:,2,i-1)).^2;
+            k=find(dist==min(dist(:)));
+            index(i,j)=index(i-1,k);
+            
+        %% below is for correlation
             prevObjects{i,j} = objectCrop{i-1,j};
                 for k = 1:height(centroids)
                     size1 = size(cell2mat(objectCrop{i,k}));
