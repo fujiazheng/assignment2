0fc43af9eccc4440306f94a9000f1757ae90ec85
Luke M <43711877+StarGalactic@users.noreply.github.com>
2024-04-08 02:27:23 -0400

    Fixed false positive and negative

--- Ass3.m
+++ Ass3.m
@@ -19,6 +19,7 @@ L = zeros(512,512,18);
 coord = zeros(12,2,18);
 objectCrop = cell(numFrame,12);
 prevObjects = cell(numFrame-1,12);
+GT_table = readtable("ground_truth_positions.xlsx");
 
 for i=1:numFrame
     frame(:,:,i)=imread("Simulate_movie_hw2.tif",i);
@@ -39,6 +40,7 @@ for i=1:numFrame
 
     hold on
     plot(centroids(:,1,i),centroids(:,2,i),'*b')
+    plot(table2array(GT_table(i*1:18:216,4)),table2array(GT_table(i*1:18:216,3)), '*r')
     hold off
     L(:,:,i) = bwlabel(logical(denoisedFrame2(:,:,i)),8);
 
@@ -79,7 +81,7 @@ for i=1:numFrame
     list = cat(1,list, orderedCentroids(:,:,i));
     end
 
-IDvector(:,:,i) = horzcat(centroids(:,:,i),sizes(:,:,i)); % create ID vector with centroids (x,y pos) and sizes (x,y pos of bounding box and x,y size)
+IDvector(:,:,i) = horzcat(orderedCentroids(:,:,i),sizes(:,:,i)); % create ID vector with centroids (x,y pos) and sizes (x,y pos of bounding box and x,y size)
 
         for c=2:12
             for d = 1:12
@@ -101,48 +103,47 @@ for z=2:12
 reorder = cat(1, reorder, list(z:12:end,:));
 end
 
-GT_table = readtable("ground_truth_positions.xlsx");
+
 
 errorX = ((reorder(:,1)-GT_table(:,4))./GT_table(:,4)).*100;
 errorY = ((reorder(:,2)-GT_table(:,3))./GT_table(:,3)).*100;
 
 % Initialize false negative count
 false_negative_count = 0;
-distance = zeros(12,12);
+false_positive_count = 0;
+distance = zeros(12,1);
 
 % Iterate through each frame
 for i = 1:numFrame
     % Iterate through each object in the current frame
     for j = 1:numObjects
         % Get position and size of the current object
-        obj_x = IDvector(j, 1, i);
-        obj_y = IDvector(j, 2, i);
-        
-        % Iterate through other objects in the same frame
+        obj_x = IDvector(:, 1, i);
+        obj_y = IDvector(:, 2, i);
         for k = 1:numObjects
-            if i>1 
-                % Get position of the other object
-                other_obj_x = GT_table{k,4};
-                other_obj_y = GT_table{k,3};
-                
-                % Compute distance between the objects
-                distance(j,k,i) = sqrt((obj_x - other_obj_x)^2 + (obj_y - other_obj_y)^2);
-                
-                %Compute error
-                error_percentage = dist(:,j,i)/distance(:,j,i) * 100;
-
-                % Check if the distance exceeds a threshold
-                if error_percentage > 10
-                    % Increment false negative count and display message
-                    false_negative_count = false_negative_count + 1;
-                    disp(['Object ', num2str(j), ' in frame ', num2str(i), ' is too far from object ', num2str(k), '.']);
-                end
-                
-            end
+            other_obj_x(k,1) = GT_table{1*i+(k-1)*18,4};
+            other_obj_y(k,1) = GT_table{1*i+(k-1)*18,3};
         end
+        % Iterate through other objects in the same frame
+        % Compute distance between the objects
+        distance(:,:,i) = sqrt((obj_x - other_obj_x).^2 + (obj_y - other_obj_y).^2);
+        
+        %Compute error
+        error_pixel(:,:,i) = diag(distance(:,:,i));
+        
+        % Check if the distance exceeds a threshold
+        
+
     end
 end
-
+if any(any(any(error_pixel > 2))) && height(obj_y > other_obj_y)
+    % Increment false negative count and display message
+    false_positive_count = height(other_object_y)-height(obj_y);
+    disp(['There are ', num2str(false_positive_count), ' false positives']);
+elseif any(any(any(error_pixel > 2))) && height(obj_y < other_obj_y)
+    false_negative_count = false_negative_count +1;
+    disp(['There are ', num2str(false_negative_count), ' false negatives']);
+end
 % Display total false negatives
 disp(['Total false negatives: ', num2str(false_negative_count)]);
 
